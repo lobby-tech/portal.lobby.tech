966b0e7b730256e586c7944ea9c3f93eca0bc3ba
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2019-12-12 17:08:42 -0300

    Updates

---
+++ precache-manifest.421f99cb25fef679f3a7eccbef84c472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ad3f32e317fe9fac4340301f544fe8",
+    "url": "https://portal.lobby.tech/index.html"
+  },
+  {
+    "revision": "79dbae1fc20a970bf877",
+    "url": "https://portal.lobby.tech/static/js/2.e1e2f592.chunk.js"
+  },
+  {
+    "revision": "5a0d989568a936270ef6c2e0e2d0229c",
+    "url": "https://portal.lobby.tech/static/js/2.e1e2f592.chunk.js.LICENSE"
+  },
+  {
+    "revision": "73d3b24981d105fb2ea2",
+    "url": "https://portal.lobby.tech/static/js/main.82ce4643.chunk.js"
+  },
+  {
+    "revision": "d8870030a014aa0a1b53",
+    "url": "https://portal.lobby.tech/static/js/runtime-main.7f42c381.js"
+  },
+  {
+    "revision": "7f12e1c74ad751a60fdfdfc66a7b7b0b",
+    "url": "https://portal.lobby.tech/static/media/logo.7f12e1c7.png"
+  }
+]);
