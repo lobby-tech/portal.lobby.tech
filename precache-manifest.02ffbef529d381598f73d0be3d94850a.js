afa62ef04e1715ce07702192ed4e789e422e1528
Luis Zambon <luis_zambon1@hotmail.com>
2020-01-21 16:25:44 -0300

    Updates

--- precache-manifest.02ffbef529d381598f73d0be3d94850a.js
+++ precache-manifest.02ffbef529d381598f73d0be3d94850a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "648a2567f1d90f015181fc780394595b",
+    "revision": "246d7eefec01444cd331eddf32e7f8bb",
     "url": "https://portal.lobby.tech/index.html"
   },
   {
-    "revision": "e03c65b3d4ed8411f988",
-    "url": "https://portal.lobby.tech/static/js/2.cee8f8c7.chunk.js"
+    "revision": "08c154179d849c3ec592",
+    "url": "https://portal.lobby.tech/static/js/2.51842282.chunk.js"
   },
   {
     "revision": "5a0d989568a936270ef6c2e0e2d0229c",
-    "url": "https://portal.lobby.tech/static/js/2.cee8f8c7.chunk.js.LICENSE"
+    "url": "https://portal.lobby.tech/static/js/2.51842282.chunk.js.LICENSE"
   },
   {
-    "revision": "9837f1484dc93092acbb",
-    "url": "https://portal.lobby.tech/static/js/main.bb0a5d9a.chunk.js"
+    "revision": "7f5a0e68cbd16b3501af",
+    "url": "https://portal.lobby.tech/static/js/main.c6729a4b.chunk.js"
   },
   {
     "revision": "356bfb8ecda90ea7718f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://portal.lobby.tech/precache-manifest.1c2af85e5ad6f8b582043f1fb267f888.js"
+  "https://portal.lobby.tech/precache-manifest.02ffbef529d381598f73d0be3d94850a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://portal.lobby.tech/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
