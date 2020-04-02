6cac3da9eb2efbbfcddfd13c5e09960fd4820bc3
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2020-04-02 16:23:22 -0300

    Updates

---
+++ precache-manifest.af02f99209dda8a83c7c6231a545139c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eab7af22b4b49620c14ae77be302c24",
+    "url": "https://portal.lobby.tech/index.html"
+  },
+  {
+    "revision": "44d8a57c042485c7f29b",
+    "url": "https://portal.lobby.tech/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "1fd757cf6e3ef7911457",
+    "url": "https://portal.lobby.tech/static/css/main.a7f3347b.chunk.css"
+  },
+  {
+    "revision": "44d8a57c042485c7f29b",
+    "url": "https://portal.lobby.tech/static/js/2.e3c64e65.chunk.js"
+  },
+  {
+    "revision": "35a08d2e06945b779fadacb826071be3",
+    "url": "https://portal.lobby.tech/static/js/2.e3c64e65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd757cf6e3ef7911457",
+    "url": "https://portal.lobby.tech/static/js/main.0214da09.chunk.js"
+  },
+  {
+    "revision": "5db8df04bba0a7d8cb00",
+    "url": "https://portal.lobby.tech/static/js/runtime-main.ca8ac9f0.js"
+  },
+  {
+    "revision": "7f12e1c74ad751a60fdfdfc66a7b7b0b",
+    "url": "https://portal.lobby.tech/static/media/logo.7f12e1c7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://portal.lobby.tech/precache-manifest.21f98af9524d376ab3b57a0f17c171c8.js"
+  "https://portal.lobby.tech/precache-manifest.af02f99209dda8a83c7c6231a545139c.js"
 );
 
 self.addEventListener('message', (event) => {
