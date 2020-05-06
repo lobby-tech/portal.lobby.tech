380ebf9cb32a1fdf8463e0ee6bae6c8989fcc09e
Kleber Nascimento Gueriero <kleber.gueriero@gmail.com>
2020-05-06 14:56:47 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://portal.lobby.tech/precache-manifest.cb653ea7975c922a5d0cbecabaa5a981.js"
+  "https://portal.lobby.tech/precache-manifest.44b39fcbd24765c94b26c39e2a048087.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://portal.lobby.tech/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
