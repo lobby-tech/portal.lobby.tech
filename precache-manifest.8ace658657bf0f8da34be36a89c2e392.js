41c6b851b29a0f69419d2567c4d0f8b855e78ad2
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2020-06-16 16:53:49 -0300

    Updates

---
+++ precache-manifest.8ace658657bf0f8da34be36a89c2e392.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c99853114bfa425ddbb53e1966f0d87d",
+    "url": "https://portal.lobby.tech/index.html"
+  },
+  {
+    "revision": "308123a3222f3e8d7204",
+    "url": "https://portal.lobby.tech/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "70f6a398eae87de7b19d",
+    "url": "https://portal.lobby.tech/static/css/main.a7f3347b.chunk.css"
+  },
+  {
+    "revision": "308123a3222f3e8d7204",
+    "url": "https://portal.lobby.tech/static/js/2.018e0a2a.chunk.js"
+  },
+  {
+    "revision": "d6289a59dececc980ee92f246f269a82",
+    "url": "https://portal.lobby.tech/static/js/2.018e0a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70f6a398eae87de7b19d",
+    "url": "https://portal.lobby.tech/static/js/main.f042b222.chunk.js"
+  },
+  {
+    "revision": "2703ef68bcfcc2e428ce",
+    "url": "https://portal.lobby.tech/static/js/runtime-main.a8ce8aa5.js"
+  },
+  {
+    "revision": "7f12e1c74ad751a60fdfdfc66a7b7b0b",
+    "url": "https://portal.lobby.tech/static/media/logo.7f12e1c7.png"
+  },
+  {
+    "revision": "9c4865bb5d21b5b05bad9760a7851e31",
+    "url": "https://portal.lobby.tech/static/media/slack-integration.9c4865bb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://portal.lobby.tech/precache-manifest.520ec332dd1658a3202748c539ca1418.js"
+  "https://portal.lobby.tech/precache-manifest.8ace658657bf0f8da34be36a89c2e392.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://portal.lobby.tech/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
