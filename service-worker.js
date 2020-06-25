e4b3a3e37addb4f5f6d36d6b4c9715db969d264c
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2020-06-25 15:02:55 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://portal.lobby.tech/precache-manifest.c5824c5e4867ebcc43679889c5bab0b0.js"
+  "https://portal.lobby.tech/precache-manifest.6496b3f5a56ab48fb560fb6ec4ee399f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://portal.lobby.tech/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
