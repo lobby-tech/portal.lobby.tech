4783f50758c1a4b54a40212f8a9bf0f2309e7244
Luis <luis_zambon1@hotmail.com>
2020-07-22 16:50:39 -0300

    Updates

---
+++ precache-manifest.8231814072f68bbbce05d9988f061942.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3984cb0d860b7784f6fd8d2f5f03f4",
+    "url": "https://portal.lobby.tech/index.html"
+  },
+  {
+    "revision": "8525b8bd1cb4c7060bfa",
+    "url": "https://portal.lobby.tech/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "4fd456d0f1e0f48b61c7",
+    "url": "https://portal.lobby.tech/static/css/main.a7f3347b.chunk.css"
+  },
+  {
+    "revision": "8525b8bd1cb4c7060bfa",
+    "url": "https://portal.lobby.tech/static/js/2.ab237334.chunk.js"
+  },
+  {
+    "revision": "d6289a59dececc980ee92f246f269a82",
+    "url": "https://portal.lobby.tech/static/js/2.ab237334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fd456d0f1e0f48b61c7",
+    "url": "https://portal.lobby.tech/static/js/main.e88d85b0.chunk.js"
+  },
+  {
+    "revision": "2703ef68bcfcc2e428ce",
+    "url": "https://portal.lobby.tech/static/js/runtime-main.a8ce8aa5.js"
+  },
+  {
+    "revision": "7f12e1c74ad751a60fdfdfc66a7b7b0b",
+    "url": "https://portal.lobby.tech/static/media/logo.7f12e1c7.png"
+  },
+  {
+    "revision": "2e40814cf41f89d334d2854b25d249ef",
+    "url": "https://portal.lobby.tech/static/media/service.2e40814c.svg"
+  },
+  {
+    "revision": "9c4865bb5d21b5b05bad9760a7851e31",
+    "url": "https://portal.lobby.tech/static/media/slack-integration.9c4865bb.png"
+  },
+  {
+    "revision": "2ea8e44783b2da340a5432aa59dde92f",
+    "url": "https://portal.lobby.tech/static/media/ticket.2ea8e447.svg"
+  }
+]);
