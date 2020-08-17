79ce3e25d9764e252928652797e3969a1f8f835e
Luis <luis_zambon1@hotmail.com>
2020-08-17 14:15:32 -0300

    Updates

---
+++ precache-manifest.63e2d0df1f118ee2a2d0f53b8d408cd4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb68bb6c79a32db027da9d1aa220953d",
+    "url": "https://portal.lobby.tech/index.html"
+  },
+  {
+    "revision": "f9b2b0351ceff7ad75f0",
+    "url": "https://portal.lobby.tech/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "9d088c39e7a6441ca0f9",
+    "url": "https://portal.lobby.tech/static/css/main.a7f3347b.chunk.css"
+  },
+  {
+    "revision": "f9b2b0351ceff7ad75f0",
+    "url": "https://portal.lobby.tech/static/js/2.5172d9f1.chunk.js"
+  },
+  {
+    "revision": "d6289a59dececc980ee92f246f269a82",
+    "url": "https://portal.lobby.tech/static/js/2.5172d9f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d088c39e7a6441ca0f9",
+    "url": "https://portal.lobby.tech/static/js/main.03a2109a.chunk.js"
+  },
+  {
+    "revision": "2703ef68bcfcc2e428ce",
+    "url": "https://portal.lobby.tech/static/js/runtime-main.a8ce8aa5.js"
+  },
+  {
+    "revision": "7f12e1c74ad751a60fdfdfc66a7b7b0b",
+    "url": "https://portal.lobby.tech/static/media/logo.7f12e1c7.png"
+  },
+  {
+    "revision": "2e40814cf41f89d334d2854b25d249ef",
+    "url": "https://portal.lobby.tech/static/media/service.2e40814c.svg"
+  },
+  {
+    "revision": "9c4865bb5d21b5b05bad9760a7851e31",
+    "url": "https://portal.lobby.tech/static/media/slack-integration.9c4865bb.png"
+  },
+  {
+    "revision": "2ea8e44783b2da340a5432aa59dde92f",
+    "url": "https://portal.lobby.tech/static/media/ticket.2ea8e447.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://portal.lobby.tech/precache-manifest.8a95a1757b0c87c9fd60c6b577ca104e.js"
+  "https://portal.lobby.tech/precache-manifest.63e2d0df1f118ee2a2d0f53b8d408cd4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://portal.lobby.tech/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
