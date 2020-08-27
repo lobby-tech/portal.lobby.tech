4b48b7983668fe6fb250d7d25835de388c0a90ba
Luis <luis_zambon1@hotmail.com>
2020-08-27 19:11:59 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://portal.lobby.tech/precache-manifest.1e33c83f811ea7b56af40de46a54edf4.js"
+  "https://portal.lobby.tech/precache-manifest.ecb39b995f286622874a3c535942ae05.js"
 );
 
 self.addEventListener('message', (event) => {
