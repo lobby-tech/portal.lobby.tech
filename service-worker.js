f26d19730d5c0cfaace8b765cafae757ae941670
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2020-11-24 17:57:57 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://portal.lobby.tech/precache-manifest.1daef5a9a3567be3efe6d2f463205d2a.js"
+  "https://portal.lobby.tech/precache-manifest.91b53e57d176a1ffe952c80163ad02dd.js"
 );
 
 self.addEventListener('message', (event) => {
