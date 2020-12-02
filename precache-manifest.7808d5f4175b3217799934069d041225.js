8eabd360d7b6f0330fa4ce969bba5cb630daef9f
Kleber Nascimento Gueriero <kleber.gueriero@gmail.com>
2020-12-02 21:37:16 +0000

    Updates

---
+++ precache-manifest.7808d5f4175b3217799934069d041225.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cd0c266f17a8bca494fba33aecbbbe",
+    "url": "https://portal.lobby.tech/index.html"
+  },
+  {
+    "revision": "6e7a2f611a1bd93af117",
+    "url": "https://portal.lobby.tech/static/js/2.253bdbb9.chunk.js"
+  },
+  {
+    "revision": "7cdd569dfb12bec9300b46f0c1cd605a",
+    "url": "https://portal.lobby.tech/static/js/2.253bdbb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cf281a1416077cd95ca",
+    "url": "https://portal.lobby.tech/static/js/main.0574e351.chunk.js"
+  },
+  {
+    "revision": "3e7cbdf74638a77c3397",
+    "url": "https://portal.lobby.tech/static/js/runtime-main.df94499f.js"
+  },
+  {
+    "revision": "7f12e1c74ad751a60fdfdfc66a7b7b0b",
+    "url": "https://portal.lobby.tech/static/media/logo.7f12e1c7.png"
+  },
+  {
+    "revision": "2e40814cf41f89d334d2854b25d249ef",
+    "url": "https://portal.lobby.tech/static/media/service.2e40814c.svg"
+  },
+  {
+    "revision": "9c4865bb5d21b5b05bad9760a7851e31",
+    "url": "https://portal.lobby.tech/static/media/slack-integration.9c4865bb.png"
+  },
+  {
+    "revision": "2ea8e44783b2da340a5432aa59dde92f",
+    "url": "https://portal.lobby.tech/static/media/ticket.2ea8e447.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://portal.lobby.tech/precache-manifest.c1009b8654a5ff87065eb4b57266884d.js"
+  "https://portal.lobby.tech/precache-manifest.7808d5f4175b3217799934069d041225.js"
 );
 
 self.addEventListener('message', (event) => {
