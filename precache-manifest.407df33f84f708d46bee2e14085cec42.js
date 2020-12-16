83adfd0a19344d39002c9fbdadd0c4e61d22cdd9
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2020-12-16 15:12:41 -0300

    Updates

---
+++ precache-manifest.407df33f84f708d46bee2e14085cec42.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ca9569e7e6139169047f1f92e7e0d9",
+    "url": "https://portal.lobby.tech/index.html"
+  },
+  {
+    "revision": "3655b5acbf548f0f87c8",
+    "url": "https://portal.lobby.tech/static/js/2.2fe23e6b.chunk.js"
+  },
+  {
+    "revision": "7cdd569dfb12bec9300b46f0c1cd605a",
+    "url": "https://portal.lobby.tech/static/js/2.2fe23e6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "038c05185a157ca95d41",
+    "url": "https://portal.lobby.tech/static/js/main.631bcb03.chunk.js"
+  },
+  {
+    "revision": "3e7cbdf74638a77c3397",
+    "url": "https://portal.lobby.tech/static/js/runtime-main.df94499f.js"
+  },
+  {
+    "revision": "7f12e1c74ad751a60fdfdfc66a7b7b0b",
+    "url": "https://portal.lobby.tech/static/media/logo.7f12e1c7.png"
+  },
+  {
+    "revision": "2e40814cf41f89d334d2854b25d249ef",
+    "url": "https://portal.lobby.tech/static/media/service.2e40814c.svg"
+  },
+  {
+    "revision": "9c4865bb5d21b5b05bad9760a7851e31",
+    "url": "https://portal.lobby.tech/static/media/slack-integration.9c4865bb.png"
+  },
+  {
+    "revision": "2ea8e44783b2da340a5432aa59dde92f",
+    "url": "https://portal.lobby.tech/static/media/ticket.2ea8e447.svg"
+  }
+]);
