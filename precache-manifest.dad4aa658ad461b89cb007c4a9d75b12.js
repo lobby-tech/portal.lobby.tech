297230a335b7381b36ae6589921dfa8553d3c0e4
Kleber Nascimento Gueriero <kleber.gueriero@gmail.com>
2021-02-09 21:35:00 +0000

    Updates

---
+++ precache-manifest.dad4aa658ad461b89cb007c4a9d75b12.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da97bab0950515c0df699eb84a27d15",
+    "url": "https://portal.lobby.tech/index.html"
+  },
+  {
+    "revision": "13ae89cbb257d7417aa8",
+    "url": "https://portal.lobby.tech/static/js/2.da04cbd0.chunk.js"
+  },
+  {
+    "revision": "256cfc5f6fd8588d932ac066d2ca7689",
+    "url": "https://portal.lobby.tech/static/js/2.da04cbd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3861c0e829a7727b8a8d",
+    "url": "https://portal.lobby.tech/static/js/3.733bf957.chunk.js"
+  },
+  {
+    "revision": "f9fe2b6d1d6ab047dc27",
+    "url": "https://portal.lobby.tech/static/js/main.e07ea6b0.chunk.js"
+  },
+  {
+    "revision": "4cb2741e86ead204b86b",
+    "url": "https://portal.lobby.tech/static/js/runtime-main.b4518561.js"
+  },
+  {
+    "revision": "7f12e1c74ad751a60fdfdfc66a7b7b0b",
+    "url": "https://portal.lobby.tech/static/media/logo.7f12e1c7.png"
+  },
+  {
+    "revision": "bdc5a6a91b331518c3b7ecc783aa7f40",
+    "url": "https://portal.lobby.tech/static/media/service-requests.bdc5a6a9.svg"
+  },
+  {
+    "revision": "2e40814cf41f89d334d2854b25d249ef",
+    "url": "https://portal.lobby.tech/static/media/service.2e40814c.svg"
+  },
+  {
+    "revision": "fdfab99ae95aa411f21142cdb43ece6d",
+    "url": "https://portal.lobby.tech/static/media/services-welcome.fdfab99a.png"
+  },
+  {
+    "revision": "9c4865bb5d21b5b05bad9760a7851e31",
+    "url": "https://portal.lobby.tech/static/media/slack-integration.9c4865bb.png"
+  },
+  {
+    "revision": "2ea8e44783b2da340a5432aa59dde92f",
+    "url": "https://portal.lobby.tech/static/media/ticket.2ea8e447.svg"
+  },
+  {
+    "revision": "1f5e7193b06b277551d83724a3c24eca",
+    "url": "https://portal.lobby.tech/static/media/tickets-welcome.1f5e7193.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://portal.lobby.tech/precache-manifest.f2cb3a6b95a38cb40fdd64d57ff2b346.js"
+  "https://portal.lobby.tech/precache-manifest.dad4aa658ad461b89cb007c4a9d75b12.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://portal.lobby.tech/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
